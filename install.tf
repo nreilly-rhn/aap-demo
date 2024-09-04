04c5e184a7c6ccaadd35b16776803dc21eb87ead
Nathan Reilly <nreilly@redhat.com>
2024-09-04 17:47:31 +1000

    .

---
+++ install.tf
@@ -0,0 +1,15 @@
+data "aws_availability_zones" "zones" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+data "aws_availability_zone" "zone" {
+  for_each = toset(data.aws_availability_zones.zones.names)
+  name = each.value
+}
+
+output "azs" {
+  value = data.aws_availability_zone.zone[*].name
+}
